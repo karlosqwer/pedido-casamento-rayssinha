e88a291f09dfdd833c180321cd4b30cf4ab17edf
karlosqwer <contatokarlos@hotmail.com>
2024-09-26 03:43:11 -0300

    finalizacao do projeto do pedido fofo de namoro para rayssinha

---
+++ script.js
@@ -0,0 +1,21 @@
+// Captura o botão de "Sim"
+const yesBtn = document.getElementById('yesBtn');
+
+// Adiciona o evento de clique ao botão "Sim"
+yesBtn.addEventListener('click', () => {
+    // Redireciona para o link do YouTube
+    window.location.href = 'https://www.youtube.com/watch?v=dWpGsK8Md28&ab_channel=LuanSantana'; 
+});
+
+// Captura o botão "Não"
+const noBtn = document.getElementById('noBtn');
+
+// Evento para fazer o botão "Não" fugir
+noBtn.addEventListener('mouseover', () => {
+    const randomX = Math.random() * window.innerWidth - noBtn.clientWidth;
+    const randomY = Math.random() * window.innerHeight - noBtn.clientHeight;
+
+    noBtn.style.position = 'absolute';
+    noBtn.style.left = `${randomX}px`;
+    noBtn.style.top = `${randomY}px`;
+});
